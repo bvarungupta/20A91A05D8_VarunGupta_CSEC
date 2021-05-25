6957ca1d62aed9484870b2c811d4c2cc2055f347
Varun Gupta <84367762+bvarungupta@users.noreply.github.com>
2021-05-25 14:25:08 +0530

    If else (elif)

---
+++ elif.py
@@ -0,0 +1,22 @@
+no=int(input("enter number: "))
+if no%2==0:
+    print("the no is divisible by 2")
+elif no%3==0:
+    print("no is divisible by 3")
+elif no%5==0:
+    printf("no is divisible by 5")
+else:
+    print("no is not divisible by 2,3 and 5")
+
+print("program completed")
+
+'''output
+4
+no is divisibl by 2
+9
+no is divisibl by 3
+15
+no is divisibl by 5
+7
+no is not divisible by 2,3 and 5
+'''
