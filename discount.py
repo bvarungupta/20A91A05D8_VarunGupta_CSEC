30d6898ed9180dfacc191d08f0b3475cfa1689b9
Varun Gupta <84367762+bvarungupta@users.noreply.github.com>
2021-05-27 09:39:18 +0530

    else if - example discounts

---
+++ discount.py
@@ -0,0 +1,15 @@
+amt=float(input('enter the amount: '))
+if(amt>0):
+    if amt>1000:
+       disc = amt*0.1
+    elif amt>2000:
+        disc=amt*0.2
+    elif amt>3000:
+        disc= amt*0.3
+    elif amt>5000:
+        dic= amt*0.4
+    else:
+         print("no discount")
+
+    print("Discount : ",disc)
+    print("Net Pay  : ",amt-disc)
