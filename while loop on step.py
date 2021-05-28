32ec543b1d4d2af0673c77a081adc28ea49e9a16
Varun Gupta <84367762+bvarungupta@users.noreply.github.com>
2021-05-28 12:31:49 +0530

    while loop using step

---
+++ while loop on step.py
@@ -0,0 +1,6 @@
+start=int(input("enter starting series: "))
+end=int(input("enter ending series: "))
+step=int(input("enter step count: "))
+while start<=end:
+    print(start,end="")
+    start=start+step
