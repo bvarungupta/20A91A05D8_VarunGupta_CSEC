b918389fa9f0f9a35659f6a276a0cd8fb5c15fb4
Varun Gupta <84367762+bvarungupta@users.noreply.github.com>
2021-05-31 12:55:14 +0530

    grading using ELIF

---
+++ grading.py
@@ -0,0 +1,19 @@
+m=int(input('enter the marks: '))
+if(m>0 and m<100):
+    if m>=80:
+          print('A')   
+
+    elif m>= 60:
+         print('B') 
+
+   elif m>=40:
+         print('C') 
+        
+    else:
+         print("failed")
+
+
+else:
+    print("invalid marks")
+
+   
