2e8b7bbc985355b65b71ee2a7907467a7bb6a1a4
Varun Gupta <84367762+bvarungupta@users.noreply.github.com>
2021-05-31 11:42:16 +0530

    Reverse of a number

---
+++ reverse of number.py
@@ -0,0 +1,11 @@
+# Python program to reverse a number
+ 
+n = int(input('enter the number: '))
+rev = 0
+ 
+while(n > 0):
+    rem = n % 10
+    rev = rev * 10 + rem
+    n = n // 10
+ 
+print('the reverse of the given number is: ',rev)
