77b4737f846170ac1e01e712935a9d248b179c8f
Varun Gupta <84367762+bvarungupta@users.noreply.github.com>
2021-06-01 09:21:36 +0530

    break statement

---
+++ break statement.py
@@ -0,0 +1,29 @@
+for i in range (1,11):
+    if i==5:
+        break
+    print(i,end=' ')
+       
+
+print('done')
+#output 1 2 3 4 done
+
+
+
+for i in range (1,11):
+    
+    print(i,end=' ')
+    if i==5:
+        break
+
+print('done')
+#done 1 2 3 4 5 done
+
+
+for i in range (1,11):
+
+    if i==5:
+      print(i,end=' ')
+      break
+print('done')
+
+#output 5 done
