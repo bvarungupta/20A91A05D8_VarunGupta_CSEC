1de14f77764801ea40eb4685820058dd27f91e30
Varun Gupta <84367762+bvarungupta@users.noreply.github.com>
2021-06-01 14:23:26 +0530

    factors - reduction of iterations

--- factors.py
+++ factors.py
@@ -1,12 +1,38 @@
-a=int(input('enter the number: '))
+n=int(input('enter the number: '))
 i=1
-while(a>=i):
-    if(a%i==0):
-        print(i,end=',')
+iterations = 0
+while(i<=n//2):
+    iterations=iterations+1
+    if(n%i==0):
+        print(i,end=' ')
+        
     i=i+1
+print(n)
+print('total iterations are ',iterations)
 
-    
-    '''output
-    enter the number 10
-    1,2,5,10
-    '''
+#output
+#enter the number: 8
+#1
+#2
+#4
+#8
+#total iterations are 8
+
+
+'''a=int(input('enter the number: '))
+i=1
+iterations = 0
+while(a>=i):
+    iterations=iterations+1
+    if(a%i==0):
+        print(i,end=' ')
+        
+    i=i+1   
+print('\n')
+print('total iterations are ',iterations)
+
+#output
+#enter the number: 8
+#1 2 4 8 
+#total iterations are  8
+'''
