21d28e229ca34773bfbcf326081bd054695443df
Varun Gupta <84367762+bvarungupta@users.noreply.github.com>
2021-06-01 09:21:56 +0530

    continue statement

---
+++ continue statement.py
@@ -0,0 +1,36 @@
+for i in range(1,11):
+    if i==5:
+        continue
+    print(i,end=' ')
+
+print('done')  
+#  output  1 2 3 4 6 7 8 9 10 done
+
+
+
+
+i=1
+while i<11:
+    if i==6:
+        break
+    print(i,end=' ')
+    i=i+1
+    
+print('done')
+#output 1 2 3 4 5 done
+
+
+
+
+i=0
+while i<10:
+    i+=1
+    if i==5:
+        continue
+    print(i,end=' ')
+    
+print('done')
+#ouput
+
+
+
