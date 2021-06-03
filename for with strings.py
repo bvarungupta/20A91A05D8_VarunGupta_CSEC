948c62d3fbfaf4d6933f7af1534c55ed8b6b3e4e
Varun Gupta <84367762+bvarungupta@users.noreply.github.com>
2021-06-03 10:28:27 +0530

    For with strings

---
+++ for with strings.py
@@ -0,0 +1,26 @@
+#for with strings
+#read input from user
+name=input('enter string: ')
+print(name)#varun
+for s in name:#if range is entered it will be displayed error as range requires integer values
+    print(s)#v,a,r,u,n
+#when using string there is no need of using RANGE
+
+
+
+'''
+S->V,S->A,S->R,S->R,S->U,S->N
+
+'''
+
+
+
+
+#OUTPUT
+'''enter string: varun
+varun
+v
+a
+r
+u
+n'''
