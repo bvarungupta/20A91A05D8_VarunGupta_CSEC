e90e3eb71cee6ebdfbc5e7880b94bc20d851c90c
Varun Gupta <84367762+bvarungupta@users.noreply.github.com>
2021-06-03 10:16:10 +0530

    sum of numbers

---
+++ sum of numbers.py
@@ -0,0 +1,33 @@
+#for loop
+#range()
+'''a=range(10)
+print(a)
+#data type of a
+print(type(a))
+'''
+
+
+
+sum1=0
+for i in range(1,11):#sum=0+1,1+2,3+1,9+4,12+5
+    sum1=sum1+i
+    print('i is',i,' and sum is',sum1)#just for tracing
+print('sum is',sum1)
+
+
+'''output
+i is 1  and sum is 1
+i is 2  and sum is 3
+i is 3  and sum is 6
+i is 4  and sum is 10
+i is 5  and sum is 15
+i is 6  and sum is 21
+i is 7  and sum is 28
+i is 8  and sum is 36
+i is 9  and sum is 45
+i is 10  and sum is 55
+sum is 55'''
+
+
+#enven sum
+#odd sum 
